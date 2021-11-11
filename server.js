52e6c274ea23f86fe53fda7a5767d93c6c27c6fe
Maria Sori <mariasori.school@gmail.com>
2021-11-11 11:04:50 -0500

    insert parties data

--- server.js
+++ server.js
@@ -117,7 +117,12 @@ app.delete('/api/candidate/:id', (req, res) => {
 
 // Create a candidate
 app.post('/api/candidate', ({ body }, res) => {
-    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    const errors = inputCheck(
+        body, 
+        'first_name', 
+        'last_name', 
+        'industry_connected'
+    );
     if (errors) {
         res.status(400).json({ error: errors });
     return;
